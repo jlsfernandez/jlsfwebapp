71e1250fcce968069371f8eef3dacaef44f52b06
JLSF <jlsfernandez@gmail.com>
2020-09-18 11:38:22 +0200

    Adding message for environmen

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AZure App Service with node.js' });
+  const data = {
+    title: 'Azure App Service Jlsf',
+    message: process.env.MESSAGE || 'THis is development'
+  };
+  //res.render('index', { title: 'AZure App Service with node.js' });
+  res.render('index',data);
 });
 
 module.exports = router;
